f65409a706dfe69a54f1678566d2164cf080094a
oldpilluwu <fawwazamin29@gmail.com>
2021-10-04 00:01:09 +0600

    leetcode dp day 2

---
+++ leetcode/746 - Min Cost Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int dp[1001];
+    int minCostClimbingStairs(vector<int>& cost) {
+        int len = cost.size()-1;
+        dp[len] = cost[len];
+        dp[len-1] = cost[len-1];
+
+        for(int i = len-2; i>=0; --i){
+            dp[i] = min(cost[i]+dp[i+1], cost[i]+dp[i+2]);
+        }
+
+        return min(dp[0], dp[1]);
+    }
+};
